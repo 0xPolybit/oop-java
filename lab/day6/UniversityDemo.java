fa013f96f2118f9c3750f9c679ed863262fec924
Swastik Biswas <swastikbiswas962@gmail.com>
2026-01-21 16:23:56 +0530

    Add University and Department classes: Implement university details and department display functionality

---
+++ lab/day6/UniversityDemo.java
@@ -0,0 +1,40 @@
+package lab.day6;
+
+class University {
+    String universityName;
+    String location;
+
+    University(String universityName, String location) {
+        this.universityName = universityName;
+        this.location = location;
+    }
+
+    class Department {
+        String deptName;
+        String hodName;
+
+        Department(String deptName, String hodName) {
+            this.deptName = deptName;
+            this.hodName = hodName;
+        }
+
+        void display() {
+            System.out.println("University: " + universityName);
+            System.out.println("Location: " + location);
+            System.out.println("Department: " + deptName);
+            System.out.println("HOD: " + hodName);
+        }
+    }
+
+    void createDepartment(String deptName, String hodName) {
+        Department dept = new Department(deptName, hodName);
+        dept.display();
+    }
+}
+
+public class UniversityDemo {
+    public static void main(String[] args) {
+        University uni = new University("KIIT University", "Bhubaneswar");
+        uni.createDepartment("Computer Science", "Dr. A. Kumar");
+    }
+}
